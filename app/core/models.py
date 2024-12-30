9229a24699df8ce34bd8e21a715b8516591e193a
yeha98555 <yeha98555@gmail.com>
2024-12-30 23:11:13 +0800

    style: Update code formatting in user class

--- app/core/models.py
+++ app/core/models.py
@@ -10,24 +10,23 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-  """Manager for users."""
+    """Manager for users."""
 
-  def create_user(self, email, password=None, **extra_fields):
-    """Create, save and return a new user."""
-    user = self.model(email=email, **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-  """User in the system."""
-  email = models.EmailField(max_length=255, unique=True)
-  name = models.CharField(max_length=255)
-  is_active = models.BooleanField(default=True)
-  is_staff = models.BooleanField(default=False)  # django admin
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # django admin
 
-  objects = UserManager()
+    objects = UserManager()
 
-  USERNAME_FIELD = 'email'
-
+    USERNAME_FIELD = 'email'

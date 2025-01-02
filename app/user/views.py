67291111d99f50d940a3ff0202562290877a2fa2
yeha98555 <yeha98555@gmail.com>
2025-01-03 01:56:54 +0800

    style: Fix code formatting

--- app/user/views.py
+++ app/user/views.py
@@ -7,5 +7,5 @@ from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-  """Create a new user in the system."""
-  serializer_class = UserSerializer
+    """Create a new user in the system."""
+    serializer_class = UserSerializer

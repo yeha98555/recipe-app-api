de0b7639eade6f3c0c5fc17c90daf8ca599a75de
yeha98555 <yeha98555@gmail.com>
2025-01-05 17:20:26 +0800

    docs: Fix docstrings typo

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -24,7 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        """Create and return a user with encrpyted password"""
+        """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
--- app/user/views.py
+++ app/user/views.py
@@ -23,7 +23,7 @@ class CreateTokenView(ObtainAuthToken):
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manage the authenticated users."""
+    """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
